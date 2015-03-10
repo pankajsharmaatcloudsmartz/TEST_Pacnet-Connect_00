12718c0e0c15f57acee3708447b5f2f656d354f6
pankshar <pankshar@IDCPANKSHAR-LT.cloudsmartz.net>
2015-03-10 13:41:32 +0530

    XD3

--- myPankjPrj/src/de/vogella/git/first/GitTest.java
+++ myPankjPrj/src/de/vogella/git/first/GitTest.java
@@ -2,6 +2,6 @@ package de.vogella.git.first;
 
 public class GitTest {
 	public static void main(String[] args) {
-	    System.out.println(" GitTest master branch pk phase 003");
+	    System.out.println(" GitTest master branch pk phase 004");
 	  }
 }
--- myPankjPrj/src/de/vogella/git/first/GitTest2.java
+++ myPankjPrj/src/de/vogella/git/first/GitTest2.java
@@ -3,7 +3,7 @@ package de.vogella.git.first;
 public class GitTest2 {
 
 	public static void main(String[] args) {
-	    System.out.println(" GitTest2 master branch pk phase 003");
+	    System.out.println(" GitTest2 master branch pk phase 004");
 	  }
 
 }
