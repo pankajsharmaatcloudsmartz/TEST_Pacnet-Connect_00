4d417145b22046561e0539d5abf49390f8a43427
pankshar <pankshar@IDCPANKSHAR-LT.cloudsmartz.net>
2015-03-10 13:04:21 +0530

    head

--- myPankjPrj/src/de/vogella/git/first/GitTest.java
+++ myPankjPrj/src/de/vogella/git/first/GitTest.java
@@ -2,6 +2,6 @@ package de.vogella.git.first;
 
 public class GitTest {
 	public static void main(String[] args) {
-	    System.out.println("Git is fun This is samepl01");
+	    System.out.println("This Is HEAD");
 	  }
 }
