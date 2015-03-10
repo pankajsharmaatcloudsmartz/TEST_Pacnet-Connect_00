d9b48d1636e1bcf8caa9914639931933f9565462
pankshar <pankshar@IDCPANKSHAR-LT.cloudsmartz.net>
2015-03-10 13:18:03 +0530

    c

--- myPankjPrj/src/de/vogella/git/first/GitTest.java
+++ myPankjPrj/src/de/vogella/git/first/GitTest.java
@@ -2,6 +2,6 @@ package de.vogella.git.first;
 
 public class GitTest {
 	public static void main(String[] args) {
-	    System.out.println("master branch pk phase 000");
+	    System.out.println("master branch pk phase 001");
 	  }
 }
